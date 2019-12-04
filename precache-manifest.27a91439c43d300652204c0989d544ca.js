a2422ed72b23a40fe9d8b83512a2909dc9cbf289
gleb <gkebkasyak@gmail.com>
2019-12-05 02:45:48 +0300

    Updates

---
+++ precache-manifest.27a91439c43d300652204c0989d544ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55a3249d1fd15542c3dba73ec9a0d1f",
+    "url": "/tetris_app/index.html"
+  },
+  {
+    "revision": "19b07ba75c9862e27481",
+    "url": "/tetris_app/static/css/main.287fef9b.chunk.css"
+  },
+  {
+    "revision": "7770605518c92ed47b09",
+    "url": "/tetris_app/static/js/2.f6bea942.chunk.js"
+  },
+  {
+    "revision": "19b07ba75c9862e27481",
+    "url": "/tetris_app/static/js/main.d8a8898b.chunk.js"
+  },
+  {
+    "revision": "800d29f0a24cfd2ed0b7",
+    "url": "/tetris_app/static/js/runtime-main.294c04aa.js"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris_app/static/media/bg.2810fe6b.png"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris_app/static/media/pixel.e2897552.woff"
+  }
+]);
